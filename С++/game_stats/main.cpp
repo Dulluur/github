ebe95b722accc7c5e863230693b4510f59512c09
Dulluur Alexandrovich <dulluur@gmail.com>
2024-01-16 20:28:46 +0900

    first commit

---
+++ С++/game_stats/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int score;
+    // double distance;
+    // char playAgain;
+    // bool shieldsUp;
+    // short int lives, aliensKilled;
+    // score = 0;
+    // distance = 1200.76;
+    // playAgain = 'y';
+    // shieldsUp = true;
+    // lives = 3;
+    // aliensKilled = 10;
+    // double engineTemp = 6572.89;
+    // cout << "\nscore: " << score << endl;
+    // cout << "distance: " << distance << endl;
+    // cout << "playAgain: " << playAgain << endl;
+
+    // cout << "lives: " << lives << endl;
+    // cout << "aliensKilled: " << aliensKilled << endl;
+    // cout << "engineTemp: " << engineTemp << endl;
+
+    // int fuel;
+    // cout << "\nHow much fuel? ";
+    // cin >> fuel;
+    // cout << "fuel: " << fuel << endl;
+    // typedef unsigned short int ushort;
+    // ushort bonus = 10;
+    // cout << "\nbonus: " << bonus << endl;
+    // return 0;
+
+    // short score = -32768;
+    // unsigned short int score = 65535;
+    // int score = 2147483647;
+    // unsigned int score = 4294967295;
+    // long score = -2147483648;
+    // unsigned long int score = 4294967295;
+    // float score = 3.4;
+    // double score = 1.7;
+    // long double score = 1.7;
+    // char score = 256;
+    // bool score = true;
+    // string score = "kaka";
+    // cout << score;
+
+    char spide = 'a';
+    cout << spide <<endl;
+
+    int score = 123;
+    cout<< ++score;
+
+    // нельзя одной переменной дать разные значения, строго типизированный язык
+}
