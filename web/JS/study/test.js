0bd16542c4e7fecbcbae69837cff3fc04faa440e
Dulluur Okoneshnikov <dulluur@gmail.com>
2024-10-19 17:20:29 +0900

    lab4-5 add

--- web/JS/study/test.js
+++ web/JS/study/test.js
@@ -1,16 +1,3 @@
-function stringChanger(str) {
-  let result = "";
-  let letters = ["а", "е", "ё", "d"];
+const num = 5
+console.log(num)
 
-  for (let i = 0; i < str.length; i++) {
-    if (letters.includes(str[i])) {
-      result = result + str[i].toUpperCase();
-    } else {
-      result = result + str[i].toLowerCase();
-    }
-  }
-
-  return result;
-}
-
-console.log(stringChanger("du ахуенный"));
