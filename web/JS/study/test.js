cee8988f72dc519e5b3ed435474b5f9d25825630
Dulluur Okoneshnikov <dulluur@gmail.com>
2024-11-04 23:02:50 +0900

    something

--- web/JS/study/test.js
+++ web/JS/study/test.js
@@ -1,4 +1,11 @@
-const num = 5
-console.log(num)
+let randomNumber = Number(trunc(Math.random() * 5) + 1)
 
-console.log(typeof(num))
+let pickNumber = Number(prompt("Попробуй отгадать число: "))
+
+if(!Number.isInteger(pickNumber) || pickNumber < 1 || pickNumber > 5){
+  alert("НЕ ТАК")
+}else if(pickNumber === randomNumber){
+  alert("Верно")
+}else{
+  alert(`Нет не ${pickNumber}, я загадол число ${randomNumber}`)
+}
