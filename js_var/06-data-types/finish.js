d6049f549dcf80021ae307a10329db292b9a9de2
Du <dulluur@gmail.com>
2025-08-02 00:14:02 +0900

    Just course assignments

---
+++ js_var/06-data-types/finish.js
@@ -0,0 +1,30 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+let myString = "Dulluur"
+let myNumber = 5
+let myBoolean = true
+let myNull = null
+let myUndefined = undefined
+let myObject = {}
+let myArray = []
+
+console.log(typeof(myString))
+console.log(typeof(myNumber))
+console.log(typeof(myBoolean))
+console.log(typeof(myNull))
+console.log(typeof(myUndefined))
+console.log(typeof(myObject))
+console.log(typeof(myArray))
